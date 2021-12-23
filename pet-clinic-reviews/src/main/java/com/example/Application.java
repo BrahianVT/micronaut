72f0d299de3a011469693d36b235b79a2aacff53
BrahianVT <pumasemj@hotmail.com>
2021-12-22 18:55:29 -0600

    added Pet Clinic review pet-clinic-reviews

--- pet-clinic-reviews/src/main/java/com/example/Application.java
+++ pet-clinic-reviews/src/main/java/com/example/Application.java
@@ -1,10 +1,25 @@
 package com.example;
 
+import com.example.utils.PetClinicReviewCliClient;
+import io.micronaut.context.event.StartupEvent;
 import io.micronaut.runtime.Micronaut;
+import io.micronaut.runtime.event.annotation.EventListener;
+import jakarta.inject.Singleton;
 
+@Singleton
 public class Application {
+    private final PetClinicReviewCliClient petClinicReviewCliClient;
+
+    public Application(PetClinicReviewCliClient petClinicReviewCliClient) {
+        this.petClinicReviewCliClient = petClinicReviewCliClient;
+    }
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
+    }
+
+    @EventListener
+    void init(StartupEvent event){
+        petClinicReviewCliClient.performDatabaseOperations();
     }
 }
---
+++ pet-clinic-reviews/src/main/java/com/example/utils/PetClinicReviewCliClient.java
@@ -0,0 +1,73 @@
+package com.example.utils;
+
+import com.example.domain.VetReview;
+import com.example.service.VetReviewService;
+import com.mongodb.client.MongoClient;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.util.CollectionUtils;
+import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Singleton
+@Requires(beans = MongoClient.class)
+public class PetClinicReviewCliClient {
+
+    private final Logger log = LoggerFactory.getLogger(PetClinicReviewCliClient.class);
+
+    private final VetReviewService vetReviewService;
+
+    public PetClinicReviewCliClient(VetReviewService vetReviewService) {
+        this.vetReviewService = vetReviewService;
+    }
+
+    public void performDatabaseOperations(){
+        performFindAll();
+        VetReview vetReview = performSave();
+        performFindByReviewId(vetReview.getReviewId());
+        performDelete(vetReview);
+        performFindAll();
+    }
+
+    public void performFindAll(){
+        log.info("--------------------------------------------");
+        log.info("-Request performFindAll-- ");
+        log.info("--------------------------------------------");
+
+        List<VetReview> vetReviews = this.vetReviewService.findAll();
+        if(CollectionUtils.isNotEmpty(vetReviews)){
+            vetReviews.forEach(vetReview -> {
+                log.info("Review :" + vetReview);
+            });
+        }
+    }
+
+    protected void performFindByReviewId(String reviewId){
+        log.info("--------------------------------------------");
+        log.info("-Request performFindByReviewId-- " + reviewId);
+        log.info("--------------------------------------------");
+
+        VetReview vetReview = vetReviewService.findByReviewId(reviewId);
+        log.info("Review :"  + vetReview);
+    }
+
+    protected VetReview performSave(){
+        VetReview vetReview = new VetReview(UUID.randomUUID().toString(), "Good experience",1L,3.5, LocalDate.now());
+
+        log.info("--------------------------------------------");
+        log.info("-Request performSave -- " + vetReview);
+        log.info("--------------------------------------------");
+        return vetReviewService.save(vetReview);
+    }
+
+    protected void performDelete(VetReview vetReview){
+        log.info("--------------------------------------------");
+        log.info("-Request performDelete-- " + vetReview);
+        log.info("--------------------------------------------");
+        vetReviewService.delete(vetReview.getReviewId());
+    }
+}
