0f66e94a3df152a303ff835b748f95e64cd1a507
BrahianVT <pumasemj@hotmail.com>
2022-01-18 14:38:06 -0600

    added consul config

--- pet-owner/src/main/java/com/pet/rest/PetResource.java
+++ pet-owner/src/main/java/com/pet/rest/PetResource.java
@@ -6,13 +6,10 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
-import io.micronaut.security.annotation.Secured;
-import io.micronaut.security.rules.SecurityRule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Controller("api")
-@Secured(SecurityRule.IS_ANONYMOUS)
 public class PetResource {
 
     private final Logger log = LoggerFactory.getLogger(PetResource.class);
